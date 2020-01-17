a3333ac6ef2752b1f9a2fda35ed7062c83288993
babymm <babymm@aliyun.com>
2020-01-17 14:13:45 +0800

    mumu-flink添加实际应用代码

--- src/main/java/com/lovecws/mumu/flink/common/mapper/dpi/CorpInfoMapper.java
+++
@@ -1,8 +0,0 @@
-package com.lovecws.mumu.flink.common.mapper.dpi;
-
-import com.lovecws.mumu.flink.common.model.dpi.CorpInfo;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface CorpInfoMapper extends BaseMapper<CorpInfo> {
-
-}
--- src/main/java/com/lovecws/mumu/flink/common/mapper/dpi/ProtocolDeviceMapper.java
+++
@@ -1,9 +0,0 @@
-package com.lovecws.mumu.flink.common.mapper.dpi;
-
-
-import com.lovecws.mumu.flink.common.model.dpi.ProtocolDevice;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface ProtocolDeviceMapper extends BaseMapper<ProtocolDevice> {
-
-}
